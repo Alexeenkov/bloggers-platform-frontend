532691d376f897ee0cc4cfe48eb27f491222c701
Ilya Alekseenkov <alekseenkov.i@promail366.com>
2025-06-13 14:27:00 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'url';
+import path from 'path';
 
 export default defineConfig({
   plugins: [vue()],
@@ -13,6 +14,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
+        includePaths: [path.resolve(__dirname, 'src')],
         additionalData: `
           @use "@/styles/_variables" as vars;
         `
